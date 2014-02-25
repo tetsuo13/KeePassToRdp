ab7dccffbc81bfa8894cd6b3c5694afd1239d93a
Andrei Nicholson <contact@andreinicholson.com>
2014-02-24 21:47:10 -0500

    Embed KeePassLib assembly

--- src/KeePassToRdp/App.xaml.cs
+++ src/KeePassToRdp/App.xaml.cs
@@ -1,8 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
+﻿#region License
+// KeePass to RDP.
+// Copyright (C) 2013-2014 Andrei Nicholson
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#endregion
+
+using System;
+using System.IO;
+using System.Reflection;
 using System.Windows;
 
 namespace KeePassToRdp
@@ -12,5 +28,23 @@ namespace KeePassToRdp
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
+                {
+                    const string resourceName = "KeePassToRdp.Resources.KeePassLib.dll";
+                    Assembly assembly = Assembly.GetExecutingAssembly();
+                    //String assemblyName = assembly.FullName;
+
+                    using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+                    {
+                        Byte[] assemblyData = new Byte[stream.Length];
+                        stream.Read(assemblyData, 0, assemblyData.Length);
+                        return Assembly.Load(assemblyData);
+                    }
+                };
+
+            base.OnStartup(e);
+        }
     }
 }
